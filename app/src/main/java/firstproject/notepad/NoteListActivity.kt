1d71705a901cf52acb6511b97d78aa7de8446221
DarkIlyas <ilyas.bouziane@um5s.net.ma>
2020-01-19 15:52:24 +0100

    Note class added

--- app/src/main/java/firstproject/notepad/NoteListActivity.kt
+++ app/src/main/java/firstproject/notepad/NoteListActivity.kt
@@ -8,5 +8,8 @@ class NoteListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
+
+
+
     }
 }
