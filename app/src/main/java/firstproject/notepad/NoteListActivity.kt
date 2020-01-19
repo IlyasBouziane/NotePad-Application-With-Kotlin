b6d1bda96f74bc07f6f69ea3fef72a7fd1117638
DarkIlyas <ilyas.bouziane@um5s.net.ma>
2020-01-19 15:45:04 +0100

    project created and configured

---
+++ app/src/main/java/firstproject/notepad/NoteListActivity.kt
@@ -0,0 +1,12 @@
+package firstproject.notepad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NoteListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_list)
+    }
+}
